26eb10c888a1b97b1135ab88948762931829bf19
J2HYD <9pvrsd@gmail.com>
2026-03-26 13:20:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const CACHE_NAME = 'j2n-quote-v1';
+const CACHE_NAME = 'j2n-quote-v5';
 const urlsToCache = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
